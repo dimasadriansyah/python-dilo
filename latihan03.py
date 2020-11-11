128c038b4cbddf708865357c5260d47d300a2e79
dimasadriansah4 <dimasadriansah4@gmail.com>
2020-11-11 15:55:14 +0700

    new files

---
+++ latihan03.py
@@ -0,0 +1,65 @@
+for x in range(5):
+	print(x)
+
+index = 0
+while index < 20:
+	if index %3 == 0:
+		print(index)
+	index += 1 #this is the same as count = count + 1
+
+count =0
+while True:
+	print(count)
+	count += 1
+	if count >= 5:
+		break
+
+for x in range(10):
+	if x % 2 == 0:
+		continue
+	print(x)
+
+for i in range(1, 10):
+	if(i%5==0):
+		break
+	print(i)
+else:
+	print("this is not printed")
+
+##exercise 3
+
+##genap
+for y in range (1, 11):
+	if y %2 == 0:
+		print(y)
+	y += 1
+
+##ganjil
+for i in range (10):
+	if i %2 == 0:
+		continue
+	print(i)
+
+##function
+
+def penjumlahan (x,y):
+	print (x + y)
+
+penjumlahan(2,3)
+
+##return
+
+def tambah (i,l):
+	c = i+l
+	return c
+hasil=tambah(5,5)
+print(hasil)
+
+def bilangan (x,y):
+	while x < y:
+		if x % 3 == 0:
+			print(x)
+		x += 1
+
+bilangan (1, 20)
+
