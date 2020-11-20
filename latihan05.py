43bd78dd8373d26d212595087676b1129ec52754
dimasadriansah4 <dimasadriansah4@gmail.com>
2020-11-20 16:01:08 +0700

    latihan05.py

---
+++ latihan05.py
@@ -0,0 +1,21 @@
+x = 279
+for i in str(x):
+    print(i)
+
+class siswa:
+    def __init__ (self, nama, umur):
+        self.nama = nama
+        self.umur = umur
+
+    def getUmur(self):
+        return self.umur
+obj = ("john", 24)
+print(obj)
+
+def number(num):
+    x = ''
+    for i in str(num):
+        x += str(int(i)**2)
+    return int(x)
+
+print(number(248))
