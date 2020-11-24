4959274807d731c463001b573ec887014ee0c7de
dimasadriansah4 <dimasadriansah4@gmail.com>
2020-11-24 16:29:16 +0700

    hari 8

--- latihan06.py
+++ latihan06.py
@@ -1,6 +1,5 @@
 def square_sum(numbers):
     return sum(i**2 for i in numbers)
 
-x = [1,2,3]
-
-print(i) for i in x
+def summation(num):
+    return sum(i for i in range(1, num+1))
---
+++ latihan08.py
@@ -0,0 +1,51 @@
+#soal 1#
+def persegi(p, l):
+    return p * l
+print(persegi(4, 2))
+
+#soal 2
+def pangkat(p):
+    return p**2
+print(pangkat(2))
+
+#soal 3
+def segitiga(x, y):
+    return 0.5 * x * y
+print(segitiga(8, 3))
+
+print('==========')
+
+num = [1,5,6,2,8]
+
+def of(x):
+    p = []
+    for i in (x):
+        y = i**2
+        p.append(y)
+    return sum(p) 
+print(of(num))
+
+def bebas(number):
+    return sum(i**2 for i in number)
+print(bebas(num))
+
+#dictionaries#
+dic = {}
+dic["john"] = 1234
+dic["jack"] = 4321
+dic["bill"] = 54321
+
+user = [
+
+        {"name" : "john",
+        "absen" : "1"},
+
+        {"name" : "luke",
+        "absen" : "2"},
+
+        {"name" : "bill",
+        "absen" : "3"},
+]
+for u in user:
+    print(u["absen"])
+print(dic)
